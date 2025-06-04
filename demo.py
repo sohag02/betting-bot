0a72480e9f4844452e083e751ae2076ae9f133ff
Sohag Jabed <sohagjabed10@gmail.com>
2025-06-05 02:22:12 +0530

    Demo balance persistance

--- demo.py
+++ demo.py
@@ -1,5 +1,10 @@
+import os
 from selenium import webdriver
 from typing import Literal
+from src.config import get_config
+import logging
+
+config = get_config()
 
 def init_status_panel(driver: webdriver.Chrome):
     driver.execute_script("""
@@ -39,3 +44,29 @@ def update_demo_balance(driver, balance: int, change_type: Literal["increase", "
             }}, 300);
         }}
     """)
+    save_last_balance(balance)
+
+def is_input_balance_changed():
+    if not os.path.exists("internal/input_balance.txt"):
+        return True
+    
+    with open("internal/input_balance.txt", "r") as f:
+        input_balance = int(f.read())
+        print(input_balance)
+        print(config.demo.assumed_balance)
+        return input_balance != config.demo.assumed_balance
+    
+def save_input_balance():
+    logging.info("Saving demo input balance")
+    with open("internal/input_balance.txt", "w") as f:
+        f.write(str(config.demo.assumed_balance))
+
+def save_last_balance(balance):
+    with open("internal/last_balance.txt", "w") as f:
+        f.write(str(balance))
+
+def get_last_demo_balance():
+    if os.path.exists("internal/last_balance.txt"):
+        with open("internal/last_balance.txt", "r") as f:
+            return int(f.read())
+    return None
--- main.py
+++ main.py
@@ -7,14 +7,17 @@ from typing import cast
 
 import undetected_chromedriver as uc
 
+from demo import (get_last_demo_balance, init_status_panel,
+                  is_input_balance_changed, save_input_balance,
+                  save_last_balance, update_demo_balance)
 from src.actions import (place_bet, press_dragon_box, press_tiger_box,
                          wait_for_results)
 from src.config import get_config
 from src.info import get_current_balance, get_last_result, get_round_id
 from src.tg import notify
-from src.utils import (BetLog, delay, is_daily_report_generated, is_eod,
-                       is_now_in_range, log_bet, save_daily_report, save_summary, generate_graphs)
-from demo import init_status_panel, update_demo_balance
+from src.utils import (BetLog, delay, generate_graphs,
+                       is_daily_report_generated, is_eod, is_now_in_range,
+                       log_bet, save_daily_report, save_summary)
 
 config = get_config()
 
@@ -52,6 +55,13 @@ class BetResult(str, Enum):
 def main():
     logging.info("Starting betting bot")
 
+    os.makedirs("internal", exist_ok=True)
+    changed = is_input_balance_changed()
+    print(changed)
+    if changed is True:
+        logging.info("Input Demo balance changed in config")
+        save_input_balance()
+
     # Make sure the log file exists
     os.makedirs("data", exist_ok=True)
     if not os.path.exists('data/betting_log.csv'):
@@ -73,115 +83,126 @@ def main():
 
     if config.demo.enabled:
         init_status_panel(driver)
-        demo_balance = config.demo.assumed_balance
+        if changed is True:
+            demo_balance = config.demo.assumed_balance
+        else:
+            demo_balance = get_last_demo_balance()
         update_demo_balance(driver, demo_balance)
+        logging.info(f"Using Demo balance: {demo_balance}")
 
     while True:
-        if is_now_in_range(config.sleep.start_time, config.sleep.end_time):
-            break
-        
-        # Generate daily report if end of day
-        if is_eod():
-            if not is_daily_report_generated():
-                save_daily_report()
-                generate_graphs()
+        try:
+            if is_now_in_range(config.sleep.start_time, config.sleep.end_time):
+                break
+            
+            # Generate daily report if end of day
+            if is_eod():
+                if not is_daily_report_generated():
+                    save_daily_report()
+                    generate_graphs()
 
-        if config.demo.enabled:
-            balance = demo_balance
-        else:
-            balance = get_current_balance(driver)
-
-        if balance and balance < bet_amt:
-            logging.info("Not enough balance to bet")
-            time.sleep(5)
-            break
-
-        if balance and balance < config.notification.balance_threshold:
-            notify(f"Balance is below threshold: {balance}")
-
-        last = get_last_result(driver)
-        time.sleep(5)
-
-        if last == "D":
-            press_dragon_box(driver)
-            bet_placed_on = BetType.DRAGON
-        elif last == "T":
-            press_tiger_box(driver)
-            bet_placed_on = BetType.TIGER
-        else:
-            # Last result is tie
-            if bet_placed_on:
-                if bet_placed_on == BetType.DRAGON:
-                    press_dragon_box(driver)
-                elif bet_placed_on == BetType.TIGER:
-                    press_tiger_box(driver)
+            if config.demo.enabled:
+                balance = demo_balance
             else:
-                # If this is first bet, bet on dragon
+                balance = get_current_balance(driver)
+
+            if balance and balance < bet_amt:
+                logging.info("Not enough balance to bet")
+                time.sleep(5)
+                break
+
+            if balance and balance < config.notification.balance_threshold:
+                notify(f"Balance is below threshold: {balance}")
+
+            last = get_last_result(driver)
+            time.sleep(5)
+
+            if last == "D":
                 press_dragon_box(driver)
                 bet_placed_on = BetType.DRAGON
-        delay()
+            elif last == "T":
+                press_tiger_box(driver)
+                bet_placed_on = BetType.TIGER
+            else:
+                # Last result is tie
+                if bet_placed_on:
+                    if bet_placed_on == BetType.DRAGON:
+                        press_dragon_box(driver)
+                    elif bet_placed_on == BetType.TIGER:
+                        press_tiger_box(driver)
+                else:
+                    # If this is first bet, bet on dragon
+                    press_dragon_box(driver)
+                    bet_placed_on = BetType.DRAGON
+            delay()
 
-        round_id = cast(int, get_round_id(driver))
-        logging.info(f"Round ID : {round_id}")
+            round_id = cast(int, get_round_id(driver))
+            logging.info(f"Round ID : {round_id}")
 
-        # calculte bet amount
-        if last_bet_status == BetResult.WON:
-            # Reset bet amount
-            bet_amt = config.betting.minimum_bet
-        elif last_bet_status == BetResult.TIE:
-            # keep bet amount same
-            bet_amt = bet_amt
-        elif last_bet_status == BetResult.LOST:
-            bet_amt = bet_amt * 2
+            # calculte bet amount
+            if last_bet_status == BetResult.WON:
+                # Reset bet amount
+                bet_amt = config.betting.minimum_bet
+            elif last_bet_status == BetResult.TIE:
+                # keep bet amount same
+                bet_amt = bet_amt
+            elif last_bet_status == BetResult.LOST:
+                bet_amt = bet_amt * 2
 
-        # place bet
-        if not config.demo.enabled:
-            status = place_bet(driver, bet_amt)
-            if not status:
-                logging.info("Bet failed, sleeping for 3 seconds")
-                time.sleep(3)
-                continue
-        wait_for_results(driver)
+            # place bet
+            if not config.demo.enabled:
+                status = place_bet(driver, bet_amt)
+                if not status:
+                    logging.info("Bet failed, sleeping for 3 seconds")
+                    time.sleep(3)
+                    continue
+            wait_for_results(driver)
 
-        # check result
-        current_result = cast(str, get_last_result(driver))
-        if bet_placed_on is not None and current_result == bet_placed_on.value:
-            last_bet_status = BetResult.WON
-            loss_streak = 0
-            logging.info(f"Won the bet of {bet_amt} points")
+            # check result
+            current_result = cast(str, get_last_result(driver))
+            if bet_placed_on is not None and current_result == bet_placed_on.value:
+                last_bet_status = BetResult.WON
+                loss_streak = 0
+                logging.info(f"Won the bet of {bet_amt} points")
+                if config.demo.enabled:
+                    demo_balance += bet_amt
+                    update_demo_balance(driver, demo_balance)
+            elif current_result == "TIE":
+                last_bet_status = BetResult.TIE
+                loss_streak = 0
+                logging.info(f"Tied with the last bet of {bet_amt} points")
+            else:
+                last_bet_status = BetResult.LOST
+                loss_streak += 1
+                logging.info(f"Lost the bet of {bet_amt} points. Streak: {loss_streak}")
+                if loss_streak >= config.notification.loss_streak_threshold:
+                    notify(f"Loss streak threshold reached: {loss_streak}")
+                if config.demo.enabled:
+                    demo_balance -= bet_amt
+                    update_demo_balance(driver, demo_balance, "decrease")
+            
             if config.demo.enabled:
-                demo_balance += bet_amt
-                update_demo_balance(driver, demo_balance)
-        elif current_result == "TIE":
-            last_bet_status = BetResult.TIE
-            loss_streak = 0
-            logging.info(f"Tied with the last bet of {bet_amt} points")
-        else:
-            last_bet_status = BetResult.LOST
-            loss_streak += 1
-            logging.info(f"Lost the bet of {bet_amt} points. Streak: {loss_streak}")
-            if loss_streak >= config.notification.loss_streak_threshold:
-                notify(f"Loss streak threshold reached: {loss_streak}")
-            if config.demo.enabled:
-                demo_balance -= bet_amt
-                update_demo_balance(driver, demo_balance, "decrease")
-        
-        if config.demo.enabled:
-            final_balance = demo_balance
-        else:
-            final_balance = get_current_balance(driver)
+                final_balance = demo_balance
+            else:
+                final_balance = get_current_balance(driver)
 
-        log_bet(
-            BetLog(
-                round_id=round_id,
-                bet_amount=bet_amt,
-                result=current_result,
-                outcome=last_bet_status.value,
-                balance=final_balance
+            log_bet(
+                BetLog(
+                    round_id=round_id,
+                    bet_amount=bet_amt,
+                    result=current_result,
+                    outcome=last_bet_status.value,
+                    balance=final_balance
+                )
             )
-        )
-        delay()
-        save_summary()
+            delay()
+            save_summary()
+        except Exception as e:
+            logging.error(e)
+            driver.save_screenshot("screenshot.png")
+            logging.error(f"Screenshot saved to screenshot.png")
+            time.sleep(5)
+            break
 
     driver.quit()
 
